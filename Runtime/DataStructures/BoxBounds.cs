72632200b35aea438c01ad84b8813363aae2b41b
Agustin Rosso <AgustinRosso_1@hotmail.com>
2025-08-09 01:08:45 +0200

    Se agregan estructuras genericas

---
+++ Runtime/DataStructures/BoxBounds.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+namespace Rossoforge.Core.DataStructures
+{
+    [Serializable]
+    public class BoxBounds<T>
+    {
+        [field: SerializeField]
+        public T Left { get; private set; }
+
+        [field: SerializeField]
+        public T Right { get; private set; }
+
+        [field: SerializeField]
+        public T Top { get; private set; }
+
+        [field: SerializeField]
+        public T Bottom { get; private set; }
+    }
+}
