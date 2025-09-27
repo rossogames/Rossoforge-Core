e709c3d12cfce321b6be9ad822e8d5e3b536341b
Agustin Rosso <AgustinRosso_1@hotmail.com>
2025-09-28 00:28:06 +0200

    nueva version Se agrega de service ui para llamadas de popups

--- Runtime/UI/IUIService.cs
+++ Runtime/UI/IUIService.cs
@@ -7,6 +7,11 @@ namespace Rossoforge.Core.UI
     public interface IUIService : IService
     {
         void CancelPopup();
+        
         T OpenPopup<T>(IPooledGameobjectData data, IPopupData popupData = null, Vector3 position = new(), Space relativeTo = Space.Self) where T : MonoBehaviour, IPopupView;
+        Awaitable<T> OpenPopup<T>(IPooledObjectAsyncData data, IPopupData popupData = null, Vector3 position = new(), Space relativeTo = Space.Self) where T : MonoBehaviour, IPopupView;
+
+        Awaitable<T> OpenPopupUntilClosed<T>(IPooledGameobjectData data, IPopupData popupData = null, Vector3 position = new(), Space relativeTo = Space.Self) where T : MonoBehaviour, IPopupView;
+        Awaitable<T> OpenPopupUntilClosed<T>(IPooledObjectAsyncData data, IPopupData popupData = null, Vector3 position = new(), Space relativeTo = Space.Self) where T : MonoBehaviour, IPopupView;
     }
 }
