6ee6fc2b6989c65fd2c629079cf9c77267fea423
Agustin Rosso <AgustinRosso_1@hotmail.com>
2025-09-27 17:18:48 +0200

    avances en implementacion de UI Service

--- Runtime/UI/IUIService.cs
+++ Runtime/UI/IUIService.cs
@@ -1,8 +1,11 @@
+using Rossoforge.Core.Pool;
 using Rossoforge.Core.Services;
+using UnityEngine;
 
 namespace Rossoforge.Core.UI
 {
     public interface IUIService : IService
     {
+        T OpenPopup<T>(IPooledGameobjectData data, IPopupData popupData = null, Vector3 position = new(), Space relativeTo = Space.Self) where T : MonoBehaviour, IPopupView;
     }
 }
