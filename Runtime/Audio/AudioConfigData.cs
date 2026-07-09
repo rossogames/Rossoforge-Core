9175c61b5da51148f922ee57b18b54a0a04fb859
Agustin Rosso <agustin.rosso.code@gmail.com>
2026-07-09 06:52:34 +0200

    se agrega Play y Stop opcional sobre el audio config

--- Runtime/Audio/AudioConfigData.cs
+++ Runtime/Audio/AudioConfigData.cs
@@ -1,13 +1,35 @@
+using System;
 using UnityEngine;
 
 namespace Rossoforge.Core.Audio
 {
     public abstract class AudioConfigData : ScriptableObject
     {
+        public event Action OnPlayRequested;
+        public event Action OnStopRequested;
+
         [field: SerializeField]
         public AudioChannelData Channel { get; private set; }
 
         [field: SerializeField]
         public MainSettings Main { get; private set; }
+
+        /// <summary>
+        /// Triggers a global request to play this audio configuration. 
+        /// All active <see cref="StaticAudioHandler"/> instances in the scene utilizing this asset will react and play simultaneously.
+        /// </summary>
+        public void Play()
+        {
+            OnPlayRequested?.Invoke();
+        }
+
+        /// <summary>
+        /// Triggers a global request to stop this audio configuration. 
+        /// All active <see cref="StaticAudioHandler"/> instances in the scene utilizing this asset will react and stop playing simultaneously.
+        /// </summary>
+        public void Stop()
+        {
+            OnStopRequested?.Invoke();
+        }
     }
 }
