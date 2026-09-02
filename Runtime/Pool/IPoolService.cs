5fa42c7004497bbaee3549b094e0007d7d16f3c1
Agustin Rosso <agustin.rosso.code@gmail.com>
2026-09-02 07:53:43 +0200

    se agrega categoria en pool

--- Runtime/Pool/IPoolService.cs
+++ Runtime/Pool/IPoolService.cs
@@ -5,17 +5,18 @@ namespace Rossoforge.Core.Pool
 {
     public interface IPoolService : IService
     {
-        T Get<T>(IPooledGameobjectData data, Transform parent, Vector3 position, Space relativeTo) where T : Component;
-        IPooledObject Get(IPooledGameobjectData data, Transform parent, Vector3 position, Space relativeTo);
-        void Populate(IPooledGameobjectData data);
+        T Get<T>(IPooledGameobjectData data, Transform parent, Vector3 position, Space relativeTo, string category) where T : Component;
+        IPooledObject Get(IPooledGameobjectData data, Transform parent, Vector3 position, Space relativeTo, string category);
+        void Populate(IPooledGameobjectData data, string category);
         void ForceReturnAll();
 
 #if HAS_ADDRESSABLES
-        Awaitable<T> GetAsync<T>(IPooledObjectAsyncData data, Transform parent, Vector3 position, Space relativeTo) where T : Component;
-        Awaitable<IPooledObject> GetAsync(IPooledObjectAsyncData data, Transform parent, Vector3 position, Space relativeTo);
-        Awaitable PopulateAsync(IPooledObjectAsyncData data);
+        Awaitable<T> GetAsync<T>(IPooledObjectAsyncData data, Transform parent, Vector3 position, Space relativeTo, string category) where T : Component;
+        Awaitable<IPooledObject> GetAsync(IPooledObjectAsyncData data, Transform parent, Vector3 position, Space relativeTo, string category);
+        Awaitable PopulateAsync(IPooledObjectAsyncData data, string category);
 #endif
 
         bool Clear(IPooledObjectData data);
+        bool Clear(string category);
     }
 }
