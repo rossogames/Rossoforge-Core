61b0ab273a5f9010bea84c3a167fa087764015fe
Agustin Rosso <agustin.rosso.code@gmail.com>
2026-09-05 06:19:44 +0200

    cambios de directorio y namespaces

---
+++ Runtime/Persistence/IPersistenceService.cs
@@ -0,0 +1,13 @@
+using Rossoforge.Services.Service;
+
+namespace Rossoforge.Persistence.Service
+{
+    public interface IPersistenceService<T> : IService where T : IPersistentData, new()
+    {
+        T Data { get; }
+
+        void Save();
+        void Load();
+        void Delete();
+    }
+}
---
+++ Runtime/Persistence/IPersistentData.cs
@@ -0,0 +1,7 @@
+namespace Rossoforge.Persistence.Service
+{
+    public interface IPersistentData
+    {
+        int Version { get; set; }
+    }
+}
